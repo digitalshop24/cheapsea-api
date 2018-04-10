108ff0a34103d158cc942fa5a1568c379bab95a0
Vitaliy Fry <vitaliy.fry@gmail.com>
2018-04-10 18:58:16 +0300

    fix offers controller

--- app/controllers/api/v1/offers_controller.rb
+++ app/controllers/api/v1/offers_controller.rb
@@ -64,7 +64,7 @@ class API::V1::OffersController < ApiController
   end
 
   def index
-    render json: Filters::OfferFilter.new(params.slice(params_array)), status: 200
+    render json: ::Filters::OfferFilter.new(params.permit(params_array)).call, status: 200
   end
 
   def create
