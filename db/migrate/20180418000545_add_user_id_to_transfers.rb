9527fd0c827f182f76c590fc3d79839b00dd09d9
Vitaliy Fry <vitaliy.fry@gmail.com>
2018-04-18 03:45:43 +0300

    change migration

--- db/migrate/20180418000545_add_user_id_to_transfers.rb
+++ db/migrate/20180418000545_add_user_id_to_transfers.rb
@@ -1,5 +1,6 @@
 class AddUserIdToTransfers < ActiveRecord::Migration[5.0]
   def change
+    Transfer.destroy_all
     add_column :transfers, :user_id, :integer, foreign_key: true, null: false
   end
 end
