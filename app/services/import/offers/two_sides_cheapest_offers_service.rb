b9305b783a821bfcaf80993613fc6369b34d105e
Vitaliy Fry <vitaliy.fry@gmail.com>
2018-04-24 06:45:24 +0300

    move worker to service

---
+++ app/services/import/offers/two_sides_cheapest_offers_service.rb
@@ -0,0 +1,80 @@
+class Import::Offers::TwoSidesCheapestOffersService
+  def call
+    cities = City.active
+    user = User.admin.take
+
+    cities.each_with_index do |origin, index|
+      @origin = origin
+
+      puts "#{origin.name} - #{index + 1}/#{cities.length} checking..."
+
+      cities.find_each do |destination|
+        @destination = destination
+
+        next if the_same_point?
+
+        fetch_offers
+
+        next if offers.empty?
+
+        # > offers
+        # > {"NYC"=>{"1"=>{"price"=>23691, "airline"=>"N7", "flight_number"=>704, "departure_at"=>"2018-09-12T06:00:00Z",
+        # NYC is destination.iata
+        offers[destination.iata].each do |offer|
+          data = offer.second
+
+          next if offer_exists?(data)
+
+          begin
+            from_google_place_service = ::ThirdParty::Geo::AutocompleteService.call(input: origin.name).result
+            to_google_place_service = ::ThirdParty::Geo::AutocompleteService.call(input: destination.name).result
+            next if from_google_place_service.nil? && to_google_place_service.nil?
+
+            Offer.create!(
+              offer_type: 'airplane',
+              two_sides: true,
+              user: user,
+              price: data['price'],
+              name: "From #{origin.name} to #{destination.name} for #{data['price']} RUB.",
+              from_google_place_id: from_google_place_service.first['place_id'],
+              to_google_place_id: to_google_place_service.first['place_id'],
+              airline: Airline.find_by(iata: data['airline']),
+              date_from: data['departure_at'],
+              date_to: data['return_at'],
+              date_end: data['expires_at']
+            )
+          rescue Exception => e
+            logger.warn e
+
+            next
+          end
+        end
+      end
+    end
+  end
+
+  private
+
+  attr_reader :origin, :destination, :offers
+
+  def the_same_point?
+    origin.id == destination.id
+  end
+
+  def fetch_offers
+    get_two_sides_offers_service = ::ThirdParty::Travelpayouts::GetTwoSidesCheapestOffersService.call(origin: origin.iata, destination: destination.iata)
+
+    context.fail!(error: get_two_sides_offers_service.error) if get_two_sides_offers_service.failure?
+
+    @offers = get_two_sides_offers_service.result
+  end
+
+  def offer_exists?(data)
+    Offer.find_by(
+      price: data['price'],
+      date_from: data['departure_at'],
+      date_to: data['return_at'],
+      two_sides: true
+    ).present?
+  end
+end
--- app/workers/import/two_sides_cheapest_offers_worker.rb
+++ app/workers/import/two_sides_cheapest_offers_worker.rb
@@ -2,60 +2,6 @@ class Import::TwoSidesCheapestOffersWorker
   include Sidekiq::Worker
 
   def perform
-    cities = City.active
-    user = User.admin.take
-
-    cities.each_with_index do |first_level_city, index|
-      puts "#{first_level_city.name} - #{index + 1}/#{cities.length} checking..."
-
-      cities.find_each do |second_level_city|
-        next if first_level_city.id == second_level_city.id
-
-        get_two_sides_offers_service = ::ThirdParty::Travelpayouts::GetTwoSidesCheapestOffersService.call(origin: first_level_city.iata, destination: second_level_city.iata)
-        context.fail!(error: get_two_sides_offers_service.error) if get_two_sides_offers_service.failure?
-
-        offers = get_two_sides_offers_service.result
-
-        next if offers.empty?
-
-        # > offers
-        # > {"NYC"=>{"1"=>{"price"=>23691, "airline"=>"N7", "flight_number"=>704, "departure_at"=>"2018-09-12T06:00:00Z",
-        # NYC is second_level_city.iata
-        offers[second_level_city.iata].each do |offer|
-          data = offer.second
-
-          next if Offer.where(
-            flight_number: data['flight_number'],
-            date_from: data['departure_at'],
-            date_to: data['return_at'],
-            two_sides: true
-          ).any?
-
-          begin
-            from_google_place_service = ::ThirdParty::Geo::AutocompleteService.call(input: first_level_city.name).result
-            to_google_place_service = ::ThirdParty::Geo::AutocompleteService.call(input: second_level_city.name).result
-            next if from_google_place_service.nil? && to_google_place_service.nil?
-
-            Offer.create!(
-              offer_type: 'airplane',
-              two_sides: true,
-              user: user,
-              price: data['price'],
-              name: "From #{first_level_city.name} to #{second_level_city.name} for #{data['price']} RUB.",
-              from_google_place_id: from_google_place_service.first['place_id'],
-              to_google_place_id: to_google_place_service.first['place_id'],
-              airline: Airline.find_by(iata: data['airline']),
-              date_from: data['departure_at'],
-              date_to: data['return_at'],
-              date_end: data['expires_at']
-            )
-          rescue Exception => e
-            logger.warn e
-
-            next
-          end
-        end
-      end
-    end
+    Import::Offers::TwoSidesCheapestOffersService.new.call
   end
 end
